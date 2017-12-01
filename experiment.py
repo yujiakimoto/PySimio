e4e23b15cdb1b24529d019b4db3128144906e2bb
Yuji Akimoto <ya242@cornell.edu>
2017-11-30 21:28:28 -0500

    Formatting

--- experiment.py
+++ experiment.py
@@ -3,6 +3,7 @@ from animation import create_map
 import pandas as pd
 from multiprocessing import Pool
 
+
 def thread_process(models):
     """ atomic process computed by each thread """
     # retrieve the arguments from the keyword-arguments
@@ -11,17 +12,17 @@ def thread_process(models):
     debug = models['debug']
     i = models['i']
 
-    m.simulate(max_time, debug = debug) # run simulation
+    m.simulate(max_time, debug=debug)   # run simulation
 
     # collect statistics
     stats = m.collect_stats()
     stats["model"] = i
 
-    m.reset()   # reset the simulation
+    m.reset()                           # reset the simulation
     return stats
 
 
-def experiment(models, max_time, iteration, output_report = True, output = 'reports.csv', debug = False):
+def experiment(models, max_time, iteration, output_report=True, output='reports.csv', debug=False):
     """ Run the experiment with input models
     Args:
         models (list) : list of map objects
@@ -33,15 +34,16 @@ def experiment(models, max_time, iteration, output_report = True, output = 'repo
     """
     assert(all(isinstance(model, Map) for model in models)), "models must be a list of Map objects"
     # begin simulations
-    print ("{} simulations with {} models begins ...".format(iteration, len(models)))
+    print("{} simulations with {} models begins ...".format(iteration, len(models)))
 
     # shared variable to combine each simulation results
     results = []
 
     for itr in range(iteration):
-        thread = Pool(len(models)) # initialize threads for each model
+        thread = Pool(len(models))  # initialize threads for each model
         # create keyword-arguments
-        args = [{'model':m, 'debug':debug, 'max_time':max_time, 'results':results, 'i':i} for i, m in enumerate(models)]
+        args = [{'model': m, 'debug': debug, 'max_time': max_time, 'results': results, 'i': i}
+                for i, m in enumerate(models)]
         stats = thread.map(thread_process, args)  # run multiprocessing
 
         for s in stats:
@@ -50,20 +52,20 @@ def experiment(models, max_time, iteration, output_report = True, output = 'repo
 
         thread.terminate()  # kill the thread
 
-    print (pd.DataFrame(results).sort_values(by = 'iteration').groupby('model').mean())
+    print(pd.DataFrame(results).sort_values(by='iteration').groupby('model').mean())
 
-    print ("experiment done")
+    print("experiment done")
 
     # generate the file
     if output_report:
         out = 'reports/'
-        pd.DataFrame(results).to_csv(out + output, index  = False)
+        pd.DataFrame(results).to_csv(out + output, index=False)
 
 
 if __name__ == '__main__':
-    num_map = 3
+    num_maps = 3
     maps = []
-    for i in range(num_map):
+    for i in range(num_maps):
         maps.append(create_map())
 
-    experiment(maps, 500, 10, output_report = False)
+    experiment(maps, 500, 10, output_report=False)
